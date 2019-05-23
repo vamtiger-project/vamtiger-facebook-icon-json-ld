c3037aad11e300b4d9abce168630ed79c1a0492f
James-Alex Matthews <vamtiger@vamtiger.com>
2019-05-23 21:41:58 +0200

    vamtiger-node-typescript-commit

---
+++ source/json-ld.ts
@@ -0,0 +1,18 @@
+const jsonLd = {
+    "@context": "http://schema.org",
+    '@type': 'ImageObject',
+    name: 'VAMTIGER Facebook HTML/SVG Icon',
+    description: 'VAMTIGER Facebook HTML/SVG Icon',
+    author: {
+        '@type': 'Person',
+        givenName: 'Vamtiger',
+        familyName: 'Immortalles',
+        email: 'vamtiger@vamtiger.com'
+    },
+    datePublished: '23 May 2018',
+    contentUrl: 'https://unpkg.com/vamtigerr-twitter-icon-json-ld/facebook.html'
+};
+
+export default [
+    jsonLd
+];
